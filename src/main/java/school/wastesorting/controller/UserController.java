86c6ce8f387563cdd400bae2d10bbf085caaf5b6
B16041310 <3364811758@qq.com>
2019-12-26 16:51:36 +0800

    add login

--- src/main/java/school/wastesorting/controller/UserController.java
+++ src/main/java/school/wastesorting/controller/UserController.java
@@ -1,11 +1,13 @@
 package school.wastesorting.controller;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import school.wastesorting.domain.User;
 import school.wastesorting.domain.Result;
+import school.wastesorting.enums.ErrorCode;
 import school.wastesorting.repository.UserRepository;
 import school.wastesorting.service.UserService;
 import school.wastesorting.util.ResultUtil;
@@ -30,5 +32,17 @@ public class UserController {
         return ResultUtil.success(userRepository.save(user));
     }
 
+    @GetMapping("/login")
+    public Result<User> Login(@RequestBody User user){
+        logger.info("login");
+        User true_user;
+        true_user = userRepository.findByName(user.getName());
+        if(true_user == null)
+            return ResultUtil.fail(ErrorCode.USER_NOT_FOUND);
+        else if(!true_user.getPassword().equals(user.getPassword()))
+            return ResultUtil.fail(ErrorCode.PASSWORD_ERROR);
+        else
+            return ResultUtil.success(ErrorCode.SUCCESS);
+    }
 
 }
