4cc4765f4066428463ebe018857c611c5653067f
胡君 <1209226858@qq.com>
2020-03-17 21:51:53 +0800

    轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: require('@/assets/img/leifengta.jpeg')
+      },
+      {
+        id: '002',
+        imgUrl: require('@/assets/img/yunqizhujing.jpeg')
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 80%
+    .swiper-img
+      width: 100%
+      background: #ccc
+</style>
